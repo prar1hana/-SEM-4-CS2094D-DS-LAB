2e5ceae0fbbb5cd2f111b26ac537707936a4b141
Prarthana Phukan <152702943+prar1hana@users.noreply.github.com>
2024-04-02 16:26:33 +0530

    Update ASSGC3_A_B220475CS_CS02_PRARTHANA_1.c

--- ASSGC3_A_B220475CS_CS02_PRARTHANA/ASSGC3_A_B220475CS_CS02_PRARTHANA/ASSGC3_A_B220475CS_CS02_PRARTHANA_1.c
+++ ASSGC3_A_B220475CS_CS02_PRARTHANA/ASSGC3_A_B220475CS_CS02_PRARTHANA/ASSGC3_A_B220475CS_CS02_PRARTHANA_1.c
@@ -1,170 +1,275 @@
+/////////////////QUES1/////////////////////////////////
+//////////////////////////////////////////////////////////
+//MADE BY PRARTHANA////////B220475CS/////////////////////
+////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_NODES 1000
+typedef struct Node 
+{
+    int destination;
+    struct Node* next;
+} Node;
 
-int adj[MAX_NODES][MAX_NODES];
-int visited[MAX_NODES];
-int component_sizes[MAX_NODES];
-int num_nodes;
-int num_components;
-int time;
-int discovery_time[MAX_NODES];
-int lowest_time[MAX_NODES];
-int is_articulation_point[MAX_NODES];
+typedef struct Graph 
+{
+    int V;
+    Node** adjList;
+} Graph;
 
-int min(int a, int b) {
-    return a < b ? a : b;
+typedef struct list 
+{
+    int v;
+    struct list* next;
+} List;
+
+List* Make(int k) 
+{
+    List* x = (List*)malloc(sizeof(List));
+    x->v = k;
+    x->next = NULL;
+    return x;
 }
 
-void dfs(int node, int parent) {
-    visited[node] = 1;
-    discovery_time[node] = lowest_time[node] = ++time;
-    int child_count = 0;
-    for (int i = 0; i < num_nodes; ++i) {
-        if (adj[node][i]) {
-            if (!visited[i]) {
-                ++child_count;
-                dfs(i, node);
-                lowest_time[node] = min(lowest_time[node], lowest_time[i]);
-                if (lowest_time[i] >= discovery_time[node] && parent != -1)
-                    is_articulation_point[node] = 1;
-            } else if (i != parent) {
-                lowest_time[node] = min(lowest_time[node], discovery_time[i]);
+Node* MakeNode(int dest) 
+{
+    Node* newNode= (Node*)malloc(sizeof(Node));
+    newNode->destination = dest;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Graph* MakeGraph(int V) {
+    Graph* graph =(Graph*)malloc(sizeof(Graph));
+    graph->V = V;
+    graph->adjList =(Node**)malloc((V + 1) * sizeof(Node));
+
+    for (int vertex=1; vertex <= V; ++vertex)
+        graph->adjList[vertex] = NULL;
+
+    return graph;
+}
+
+void AddingEdges(Graph* graph, int src, int dest) {
+    Node* newNode = MakeNode(dest);
+    newNode->next = graph->adjList[src];
+    graph->adjList[src] = newNode;
+}
+// Depth First Search (DFS) traversal starting from vertex v.
+void DepthFirstSearchUtilityfunction(int v, Graph* graph, int isVisited[]) {
+    isVisited[v] = 1;
+    Node* temp = graph->adjList[v];
+
+    while (temp) {
+        int adjVertex = temp->destination;
+        if (!isVisited[adjVertex])
+            DepthFirstSearchUtilityfunction(adjVertex, graph, isVisited);
+        temp = temp->next;
+    }
+}
+
+int ConnectedComponentsNumber(Graph* graph) {
+    int V = graph->V;
+    int* isVisited = (int*)malloc((V + 1) * sizeof(int));
+    int count = 0;
+
+    for (int vertex = 1; vertex <= V; ++vertex)
+        isVisited[vertex] = 0;
+
+    for (int vertex = 1; vertex <= V; ++vertex) {
+        if (!isVisited[vertex]) {
+            DepthFirstSearchUtilityfunction(vertex, graph, isVisited);
+            ++count;
+        }
+    }
+
+    free(isVisited);
+    return count;
+}
+// (dfs traversal to calc the size of each connected component)
+void DepthFirstSearchUtilityfunction2(int v, Graph* graph, int isVisited[], int* count) {
+    isVisited[v] = 1;
+    Node* temp = graph->adjList[v];
+
+    while (temp) {
+        int adjVertex = temp->destination;
+        if (!isVisited[adjVertex]) {
+            (*count)++;
+            DepthFirstSearchUtilityfunction2(adjVertex, graph, isVisited, count);
+        }
+        temp = temp->next;
+    }
+}
+
+void SIZE(Graph* graph) {
+    int V = graph->V;
+    int* isVisited = (int*)malloc((V + 1) * sizeof(int));
+    List* li = NULL;
+
+    for (int vertex = 1; vertex <= V; ++vertex)
+        isVisited[vertex] = 0;
+
+    for (int vertex = 1; vertex <= V; ++vertex) {
+        if (!isVisited[vertex]) {
+            int count = 0;
+            DepthFirstSearchUtilityfunction2(vertex, graph, isVisited, &count);
+            List* x = Make(count + 1);
+            List* prev = NULL;
+            List* curr = li;
+
+            while (curr && curr->v <= x->v) {
+                prev = curr;
+                curr = curr->next;
+            }
+
+            if (prev == NULL) {
+                x->next = li;
+                li = x;
+            } else {
+                prev->next = x;
+                x->next = curr;
             }
         }
     }
-    if (parent == -1 && child_count > 1)
-        is_articulation_point[node] = 1;
+    List* cu = li;
+    while (cu) {
+        printf("%d ", cu->v);
+        cu = cu->next;
+    }
+    printf("\n");
+    free(isVisited);
+}
+//performs DFS traversal nd cals lowLink nd timeStamp values for each vertex 
+//to find bridges in graph.
+void dfs3(Graph* graph, int vertex, int* lowLink, int* timeStamp, int* isVisited, int* c, int p, int* time) {
+    isVisited[vertex] = 1;
+    lowLink[vertex] = *time;
+    timeStamp[vertex] = *time;
+    (*time)++;
+    Node* temp = graph->adjList[vertex];
+    while (temp != NULL) {
+        if (temp->destination == p) {
+            temp = temp->next;
+            continue;
+        }
+        if (!isVisited[temp->destination]) {
+            dfs3(graph, temp->destination, lowLink, timeStamp, isVisited, c, vertex, time);
+            if (lowLink[vertex] > lowLink[temp->destination])
+                lowLink[vertex] = lowLink[temp->destination];
+            if (lowLink[temp->destination] > timeStamp[vertex])
+                (*c) = (*c) + 1;
+        } else {
+            if (lowLink[vertex] > lowLink[temp->destination])
+                lowLink[vertex] = lowLink[temp->destination];
+        }
+
+        temp = temp->next;
+    }
 }
 
-void dfs_bridge(int node, int parent) {
-    visited[node] = 1;
-    for (int i = 0; i < num_nodes; ++i) {
-        if (adj[node][i]) {
-            if (!visited[i]) {
-                dfs_bridge(i, node);
-            } else if (i != parent && discovery_time[i] < discovery_time[node]) {
-                lowest_time[node] = min(lowest_time[node], discovery_time[i]);
+void NumberOfArticulationPoints(Graph* graph, int vertex, int* lowLink, int* timeStamp, int* isVisited, int* articulationPoints, int p, int* time) {
+    int child = 0;
+    isVisited[vertex] = 1;
+    lowLink[vertex] = *time;
+    timeStamp[vertex] = *time;
+    (*time)++;
+    Node* temp = graph->adjList[vertex];
+    while (temp != NULL) {
+        if (!isVisited[temp->destination]) {
+            child++;
+            NumberOfArticulationPoints(graph, temp->destination, lowLink, timeStamp, isVisited, articulationPoints, vertex, time);
+            if (lowLink[vertex] > lowLink[temp->destination])
+                lowLink[vertex] = lowLink[temp->destination];
+            if (lowLink[temp->destination] >= timeStamp[vertex] && p != -1) {
+                articulationPoints[vertex] = 1;
+            }
+        } else if (temp->destination != p) {
+            if (lowLink[vertex] > timeStamp[temp->destination])
+                lowLink[vertex] = timeStamp[temp->destination];
+        }
+        temp = temp->next;
+    }
+    if (child > 1 && p == -1) {
+        articulationPoints[vertex] = 1;
+    }
+}
+
+int main(void) {
+    int V, vertex, j, c;
+    char s[1000];
+    int a[100];
+    scanf("%d", &V);
+    Graph* graph = MakeGraph(V);
+    for (vertex = 0; vertex < V; vertex++) {
+        scanf(" %[^\n]s", s);
+        c = 0;
+        j = 0;
+        while (s[j] != '\0') {
+            int num = 0;
+            while (s[j] >= '0' && s[j] <= '9') {
+                num = num * 10 + (s[j] - '0');
+                j++;
+            }
+            a[c++] = num;
+            if (s[j] == ' ') {
+                j++;
             }
         }
-    }
-}
-
-int find_bridges() {
-    int bridge_count = 0;
-    for (int i = 0; i < num_nodes; ++i) {
-        if (!visited[i]) {
-            dfs_bridge(i, -1);
-        }
-        for (int j = 0; j < num_nodes; ++j) {
-            if (adj[i][j] && lowest_time[j] > discovery_time[i]) {
-                ++bridge_count;
-            }
+        for (j = 1; j < c; j++) {
+            AddingEdges(graph, vertex + 1, a[j]);
         }
     }
-    return bridge_count;
-}
-
-void dfs_components(int node) {
-    visited[node] = 1;
-    ++component_sizes[num_components];
-    for (int i = 0; i < num_nodes; ++i) {
-        if (adj[node][i] && !visited[i]) {
-            dfs_components(i);
-        }
-    }
-}
-
-int find_components() {
-    int max_size = 0;
-    for (int i = 0; i < num_nodes; ++i) {
-        if (!visited[i]) {
-            ++num_components;
-            dfs_components(i);
-            max_size = component_sizes[num_components - 1];
-        }
-    }
-    return max_size;
-}
-
-void initialize() {
-    for (int i = 0; i < MAX_NODES; ++i) {
-        for (int j = 0; j < MAX_NODES; ++j) {
-            adj[i][j] = 0;
-        }
-    }
-}
-
-int main() {
-    char choice;
-    initialize();
-    scanf("%d", &num_nodes);
-    if (num_nodes > MAX_NODES) {
-        printf("Error: Number of nodes exceeds maximum allowed.\n");
-        return 1;
-    }
-    for (int i = 0; i < num_nodes; ++i) {
-        int node, num_neighbors;
-        scanf("%d %d", &node, &num_neighbors);
-        if (num_neighbors > MAX_NODES) {
-            printf("Error: Number of neighbors exceeds maximum allowed.\n");
-            return 1;
-        }
-        for (int j = 0; j < num_neighbors; ++j) {
-            int neighbor;
-            scanf("%d", &neighbor);
-            adj[node][neighbor] = 1;
-            adj[neighbor][node] = 1; // undirected graph
-        }
-    }
+    char ch;
     while (1) {
-        scanf(" %c", &choice);
-        if (choice == 't') {
-            break;
-        } else if (choice == 'n') {
-            num_components = 0;
-            int max_size = find_components();
-            printf("%d\n", max_size);
-        } else if (choice == 's') {
-            for (int i = 0; i < MAX_NODES; ++i) {
-                component_sizes[i] = 0;
-                visited[i] = 0; // Reset visited array
+
+        scanf(" %c", &ch);
+        if (ch == 'n') {
+            printf("%d\n", ConnectedComponentsNumber(graph));
+        } else if (ch == 's') {
+            SIZE(graph);
+        } else if (ch == 'b') {
+            int isVisited[100];
+            for (vertex = 1; vertex <= V; vertex++) {
+                isVisited[vertex] = 0;
             }
-            num_components = 0;
-            int max_size = find_components();
-            printf("%d\n", num_components);
-            for (int i = 0; i < num_components; ++i) {
-                printf("%d ", component_sizes[i]);
-            }
-            printf("\n");
-        } else if (choice == 'b') {
-            for (int i = 0; i < MAX_NODES; ++i) {
-                visited[i] = 0;
-                lowest_time[i] = discovery_time[i] = 0;
-            }
-            int bridges = find_bridges();
-            printf("%d\n", bridges);
-        } else if (choice == 'a') {
-            for (int i = 0; i < MAX_NODES; ++i) {
-                visited[i] = 0;
-                is_articulation_point[i] = 0;
-            }
-            num_components = 0;
-            int max_size = find_components(); // Re-calculate components
-            for (int i = 0; i < num_nodes; ++i) {
-                if (!visited[i]) {
-                    dfs(i, -1);
+            int c = 0;
+            int lowLink[V + 1];
+            int timeStamp[V + 1];
+            int time = 1;
+            for (vertex = 1; vertex <= V; vertex++) {
+                if (!isVisited[vertex]) {
+                    time = 1;
+                    dfs3(graph, vertex, lowLink, timeStamp, isVisited, &c, -1, &time);
                 }
             }
-            int articulation_points = 0;
-            for (int i = 0; i < num_nodes; ++i) {
-                if (is_articulation_point[i]) {
-                    ++articulation_points;
+            (c != 0) ? printf("%d\n", c) : printf("-1\n");
+        } else if (ch == 'a') {
+            int articulationPoints[V + 1];
+            int isVisited[100];
+            for (vertex = 1; vertex <= V; vertex++) {
+                isVisited[vertex] = 0;
+                articulationPoints[vertex] = 0;
+            }
+            int c = 0;
+            int lowLink[V + 1];
+            int timeStamp[V + 1];
+            int time = 1;
+            for (vertex = 1; vertex <= V; vertex++) {
+                if (!isVisited[vertex]) {
+                    NumberOfArticulationPoints(graph, vertex, lowLink, timeStamp, isVisited, articulationPoints, -1, &time);
                 }
             }
-            printf("%d\n", articulation_points);
+            for (vertex = 1; vertex <= V; vertex++) {
+                if (articulationPoints[vertex] == 1) {
+                    c++;
+                }
+            }
+            (c != 0) ? printf("%d\n", c) : printf("-1\n");
+        } else if (ch == 't') {
+            exit(0);
         }
     }
-    return 0;
+
+
+    return 1;
 }
